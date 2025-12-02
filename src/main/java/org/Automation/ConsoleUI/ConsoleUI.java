ab76b32bd77dce61610ed74fc285e751dcebf174
Barok Yeshiber <93937020+Barok-y@users.noreply.github.com>
2025-12-02 22:18:02 +0300

    Add ConsoleUI class with menu and input methods

---
+++ src/main/java/org/Automation/ConsoleUI/ConsoleUI.java
@@ -0,0 +1,25 @@
+package ConsoleUI;
+import java.util.*;
+public class ConsoleUI  {
+	// Low-Level Menu print lines
+	public void printHeader(String title) {
+		
+	}
+	
+	public void printMenu(ArrayList<String> options) {
+		
+	}
+	
+	public String getUserInput() {
+		
+		return "";
+	}
+	
+	public void printStatus(String message) {
+		
+	}
+	
+	public void printError(String message) {
+		
+	}
+}
